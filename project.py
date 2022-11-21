4073f34b60fa6c99606df20bf8a4883989f3e902
valentijn <terpstravalentijn1996@gmail.com>
2022-11-21 11:35:23 +0100

    Merge remote-tracking branch 'origin/main'

--- project.py
+++ project.py
@@ -1,18 +1,18 @@
-LANG = "EN"
+def check_antwoord(antwoord):
+    return antwoord == 42
 
 def hello_world(lang):
     if lang == "NL":
-        return "Hallo wereld!"
+        print("Hallo wereld!")
     elif lang == "FR":
-        return "Bonjour le monde!"
+        print("Bonjour le monde!")
     else:
-        return "Hello world!"
+        print("Hello world!")
 
-def test():
-    assert hello_world("NL") == "Hallo wereld!", "Test failed"
+hello_world("EN")
 
-def main():
-    print(hello_world(LANG))
-
-#test()
-main()
+antwoord = input("Wat is het antwoord op de ultieme vraag van het leven, het universum, en alles?")
+if check_antwoord(antwoord):
+    print("Correct!")
+else:
+    print("Mogelijk heb je een andere vraag in je hoofd?")
